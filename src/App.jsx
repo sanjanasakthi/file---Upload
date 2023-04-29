d11fb18698874f3a11237b9ee48473a2d175ec52
sanjanasakthi <sanjanasakthi2001@gmail.com>
2023-04-29 09:37:47 +0530

    'fileUploding'

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MultiFileUpload from "./MultiFileUpload";
+function ImageUploading(){
+                return(
+                    <>
+                    {/* <div className="firstHalf"> */}
+                    <h1 className="text big ms-5 mt-5 text-danger" id="web">
+                      Image Upload Sample
+                    </h1>
+                   <MultiFileUpload/> 
+                    {/* </div> */}
+
+                   </>
+                )
+}
+export default ImageUploading;
